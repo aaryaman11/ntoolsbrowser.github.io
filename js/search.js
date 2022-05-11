0507660ce55c93c695aae514aec0f78ea509b8f8
jpb93 <jeremy.burkhardt@gmail.com>
2022-05-11 15:44:38 -0400

    Add submit on enter

--- js/search.js
+++ js/search.js
@@ -4,31 +4,34 @@
     main()
     : window.addEventListener('load', main)
 })(() => {
-  document.getElementById('submitbtn').onclick = () => {
-    const subject = document.getElementById('search-bar').value;
-    const protocol = window.location.protocol;
-    const url = `${protocol}//ievappwpdcpvm01.nyumc.org/?file=${subject}.json`;
+  document.getElementById('submitbtn').addEventListener('click', loadData);
 
-    if (urlExists(url)) {
-      window.location.href = `./view.html?mode=build&subject=${subject}`;
-    }
-    else {
-      document.getElementById('err').innerText = 'Data not found!';
-      console.log('Data not found!');
-    }
-  }
-  const subject_list = document.getElementById('list')
-  subject_list.addEventListener('change', () => {
-    if (subject_list.selectedIndex > 0) {
-      window.location.href = `./view.html?mode=demo&subject=${subject_list.value}`;
+  const subjectList = document.getElementById('list')
+  subjectList.addEventListener('change', () => {
+    if (subjectList.selectedIndex > 0) {
+      window.location.href = `./view.html?mode=demo&subject=${subjectList.value}`;
     }
   });
-
 });
+
+const loadData = () => {
+  const subject = document.getElementById('search-bar').value;
+  const protocol = window.location.protocol;
+  const url = `${protocol}//ievappwpdcpvm01.nyumc.org/?file=${subject}.json`;
+
+  if (urlExists(url)) {
+    window.location.href = `./view.html?mode=build&subject=${subject}`;
+  }
+  else {
+    document.getElementById('err').innerText = 'Data not found!';
+    console.log('Data not found!');
+  }
+}
 
 const urlExists = (url) => {
   const request = new XMLHttpRequest();
   request.open('HEAD', url, false);
   request.send();
   return request.status !== 404;
-}   
+}
+   
