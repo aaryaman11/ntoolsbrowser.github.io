477dcdaa8e8b642123cd5872eeb30d87708bfaa2
Josh Chen <30662997+jingyunc@users.noreply.github.com>
2022-08-24 09:29:59 -0400

    add comments and error-catchers

--- preprocessing/genblank_nmac.py
+++ preprocessing/genblank_nmac.py
@@ -29,19 +29,27 @@ def isComposite(type):
 if  len(sys.argv) == 2:
     subject = sys.argv[1]
 
+    # create BIDS folder
     os.makedirs(wdir + "/sub-" + subject)
     os.makedirs(wdir + "/sub-" + subject + "/ana")
     os.makedirs(wdir + "/sub-" + subject + "/ieeg")
+    
+    # extract brain surface and volume files
     ffolders = glob.glob(fdir + "/auto" + subject + "*")
+    if len(ffolders) == 0:
+        print("Freesurfer folder not found")
+        sys.exit()
     ffolders.sort(key=os.path.getmtime)
     os.system("cp " + ffolders[0] + "/surf/lh.pial " + wdir + "/sub-" + subject + "/ana/sub-" + subject + "_freesurferleft.pial")
     os.system("cp " + ffolders[0] + "/surf/rh.pial " + wdir + "/sub-" + subject + "/ana/sub-" + subject + "_freesurferright.pial")
     os.system("gunzip -c " + ldir + "/" + subject + "/T1.nii.gz > " + wdir + "/sub-" + subject + "/ana/sub-" + subject + "_preoperation_T1w.nii")
 
+    # generate json file
     cfiles = glob.glob(ldir + "/" + subject + "/*_coor_T1_*.txt")
-    cfiles = glob.glob(ldir + "/" + subject + "/*.txt")
+    if len(cfiles) == 0:
+        print("Elec coordinate text file not found")
+        sys.exit()
     cfiles.sort(key=os.path.getmtime)
- 
     coord_file = open(cfiles[0])
     coord_contents = coord_file.read()
     electrode_objects = []
@@ -74,7 +82,6 @@ if  len(sys.argv) == 2:
 
     jfile=wdir + "/sub-" + subject + "/ieeg/sub-" + subject + "_ntoolsbrowser.json"
     with open(jfile, 'w') as output_file:
-    # with open(f'sub-{subject}_ntoolsbrowser.json', 'w') as output_file:
         json.dump(final_json, output_file, ensure_ascii=False, indent=4)
 else:
     print("Invalid inputs")
